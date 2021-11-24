c43a71ca5206749e59c0184e2aa764094244d962
kechiu-dev <kqqiudev@gmail.com>
2021-11-24 17:51:17 +0800

    modify schema.sql

--- interview-assignment/src/main/resources/schema.sql
+++ interview-assignment/src/main/resources/schema.sql
@@ -6,42 +6,38 @@ DROP TABLE IF EXISTS activity;
 
 create table account
 (
-	user_id varchar(38) NOT NULL,
-	username varchar(50) NULL,
-	disable varchar(1) NULL,
-	create_date date NULL,
-	update_date date NULL
+	user_id varchar(38) PRIMARY KEY NOT NULL,
+	username varchar(50) NOT NULL,
+	disable varchar(1) NOT NULL,
+	create_date date NOT NULL,
+	update_date date NOT NULL
 );
-create unique index pk_account on account (user_id);
 
 create table activity
 (
-	activity_id varchar(38) NULL,
-	list_id varchar(38) NULL,
-	user_id varchar(38) NULL,
-	create_date date NULL,
-	update_date date NULL
+	activity_id varchar(38) PRIMARY KEY NOT NULL,
+	list_id varchar(38) NOT NULL,
+	user_id varchar(38) NOT NULL,
+	create_date date NOT NULL,
+	update_date date NOT NULL
 );
-create unique index pk_activity on activity (activity_id);
 
 create table list
 (
-	list_id varchar(38) NULL,
-    list_name varchar(50) NULL,
-	due_date date NULL,
-	create_date date NULL,
-	update_date date NULL
+	list_id varchar(38) PRIMARY KEY NOT NULL,
+    list_name varchar(50) NOT NULL,
+	due_date date NOT NULL,
+	create_date date NOT NULL,
+	update_date date NOT NULL
 );
-create unique index pk_list on list (list_id);
 
 create table item
 (
-    item_id varchar(38) NULL,
-    list_id varchar(38) NULL,
-    item_name varchar(50) NULL,
-    description varchar(100) NULL,
-    deadline date NULL,
-    create_date date NULL,
-	update_date date NULL
+    item_id varchar(38) PRIMARY KEY NOT NULL,
+    list_id varchar(38) NOT NULL,
+    item_name varchar(50) NOT NULL,
+    description varchar(100) NOT NULL,
+    deadline date NOT NULL,
+    create_date date NOT NULL,
+	update_date date NOT NULL
 );
-create unique index pk_item on item (item_id);
